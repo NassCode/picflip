f2d1165a4518247b3f5866b675058b2a8668c026
Nasser Ghaleb <63391304+NassCode@users.noreply.github.com>
2023-07-12 20:43:45 +0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let myImg = document.getElementById('myImg')
+
+console.log(myImg)
+
+
+function flipPic() {
+  myImg.src = 'https://images6.fanpop.com/image/photos/36700000/Cats-image-cats-36712807-1600-1200.jpg'
+}
